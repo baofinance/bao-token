afe1f5db6ffff196ef06bd552b8118475c03197a
Ben Clabby <benclabby@gmail.com>
2022-04-11 10:54:40 -0400

    Fix snapshot.ts

--- distribution-merkle/src/snapshot.ts
+++ distribution-merkle/src/snapshot.ts
@@ -1,9 +1,29 @@
 import fs from 'fs'
-import { utils, BigNumber } from 'ethers'
+import { ethers, utils, BigNumber } from 'ethers'
 import axios from 'axios'
+import baoV1Abi from './abi/baov1.json'
 
-const checkDupes = () => {
-  const snapshot = JSON.parse(fs.readFileSync(`${__dirname}/../snapshot.json`).toString())
+// -------------------------------
+// CONSTANTS
+// -------------------------------
+
+const SNAPSHOT_FILE = `${__dirname}/../snapshot.json`
+
+// -------------------------------
+// SNAPSHOT VERIFICATION
+// -------------------------------
+
+const verifySnapshot = async () => {
+  console.log('Checking for duplicates and computing totals...')
+  const snapshot = JSON.parse(fs.readFileSync(SNAPSHOT_FILE).toString())
+
+  const mainnetProvider = new ethers.providers.AlchemyProvider()
+  const bao = new ethers.Contract('0x374CB8C27130E2c9E04F44303f3c8351B9De61C1', baoV1Abi, mainnetProvider)
+  const xdaiProvider = new ethers.providers.JsonRpcProvider('https://rpc.gnosischain.com/')
+  const baoCx = new ethers.Contract('0xe0d0b1DBbCF3dd5CAc67edaf9243863Fd70745DA', baoV1Abi, xdaiProvider)
+
+  const mainnetLocked = await bao.lockedSupply()
+  const xdaiLocked = await baoCx.lockedSupply()
 
   const exists = []
   let total = BigNumber.from(0)
@@ -14,12 +34,24 @@ const checkDupes = () => {
     newTotal = newTotal.add(BigNumber.from(account.amount).div(1e4))
     exists.push(account.address)
   })
+
   console.log('Done!')
+  console.log(`Accounts: ${exists.length}`)
   console.log(`Total locked BAO: ${total.toString()} or ${utils.formatUnits(total.toString())}`)
   console.log(`Equivalent with new cap: ${newTotal.toString()}`)
+  console.log(
+    `Actual Locked Balances: ${
+      mainnetLocked.toString()
+    } (ETH) | ${xdaiLocked.toString()} (XDAI) | ${mainnetLocked.add(xdaiLocked).toString()} (TOTAL)`
+  )
+  console.log(`Snapshot valid?: ${mainnetLocked.add(xdaiLocked).eq(total) ? 'Yes' : 'No'}`) // Should always be yes!
 }
 
-const fetchLockedBalances = async () => {
+// -------------------------------
+// SNAPSHOT GENERATION
+// -------------------------------
+
+const takeSnapshot = async () => {
   let lockedBalances = []
 
   const getQuery = (i: number) =>
@@ -35,7 +67,10 @@ const fetchLockedBalances = async () => {
   console.log('Fetching mainnet data from subgraph...')
   for (let i = 0;;i += 1000) {
     const query = getQuery(i)
-    const { data: mainnet } = await axios.post('https://api.thegraph.com/subgraphs/name/n0xmare/locked-bao-mainnet', { query })
+    const { data: mainnet } = await axios.post(
+      'https://api.thegraph.com/subgraphs/name/n0xmare/locked-bao-mainnet',
+      { query }
+    )
     if (mainnet.errors) break
 
     lockedBalances = lockedBalances.concat(
@@ -52,7 +87,7 @@ const fetchLockedBalances = async () => {
   console.log('Fetching xdai data from subgraph and merging datasets...')
   let updated = 0
   let newAddresses = 0
-  for (let i = 1000;;i += 1000) {
+  for (let i = 0;;i += 1000) {
     const query = getQuery(i)
     const { data: xdai } = await axios.post(
       'https://api.thegraph.com/subgraphs/name/n0xmare/locked-bao-xdai',
@@ -61,7 +96,7 @@ const fetchLockedBalances = async () => {
     if (xdai.errors) break
 
     for (let j = 0; j < xdai.data.accounts.length; j++) {
-      const account = xdai.data.accounts[j];
+      const account = xdai.data.accounts[j]
       const index = mainnetAddresses.indexOf(account.id)
 
       if (index >= 0) {
@@ -80,21 +115,20 @@ const fetchLockedBalances = async () => {
     }
   }
 
-  console.log(`Done! Updated ${updated} addresses and found ${newAddresses} new addresses.`)
+  console.log(`Done! Updated balances for ${updated} addresses and found ${newAddresses} new addresses.`)
 
   lockedBalances = lockedBalances.sort((a: any, b: any): number => {
     return BigNumber.from(a.amount).gt(BigNumber.from(b.amount)) ? -1 : 1
   })
 
-  fs.writeFileSync(`${__dirname}/../snapshot.json`, JSON.stringify(lockedBalances, null, 2))
+  fs.writeFileSync(SNAPSHOT_FILE, JSON.stringify(lockedBalances, null, 2))
   console.log('Done! Results written to snapshot.json')
 }
 
 const main = async () => {
-  await fetchLockedBalances()
-
-  console.log('Checking for duplicates and computing totals...')
-  checkDupes()
+  await takeSnapshot()
+  console.log()
+  await verifySnapshot()
 }
 
 main()
